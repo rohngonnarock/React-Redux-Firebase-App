2b6951070cd28e9c4cbb450a461029179ed60e88
Rahul Prajapati <rahpraja@publicisgroupe.net>
2018-12-12 12:26:24 +0530

    file upload

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -5,13 +5,33 @@ import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
 import { Redirect } from 'react-router-dom'
-
+import FileUpload from './FileUplaod'
+import firebase from "firebase";
 
 
 class Dashboard extends Component {
+  state = {
+    avatarURL: ""
+  };
+  loadImages = () => {
+    firebase
+      .storage()
+      .ref("images")
+      .child('341e8266-682b-4be6-8a8e-8dfd450bdfd5.gif')
+      .getDownloadURL()
+      .then(url => {
+        console.log(`url -- ${url}`)
+        debugger;
+        this.setState({ avatarURL: url })
+      }).catch(err => console.log(`error -- ${err}`))
+  }
+
+
   render() {
+    this.loadImages();
     const { projects, auth, notifications } = this.props;
-    if (!auth.uid) return <Redirect to='/signin' /> 
+    console.log(`avatar ${this.props.avatarURL}`)
+    if (!auth.uid) return <Redirect to='/signin' />
 
     return (
       <div className="dashboard container">
@@ -21,6 +41,9 @@ class Dashboard extends Component {
           </div>
           <div className="col s12 m5 offset-m1">
             <Notifications notifications={notifications} />
+          </div>
+          <div className="col s12 m5 offset-m1">
+            <FileUpload></FileUpload>
           </div>
         </div>
       </div>
@@ -40,7 +63,7 @@ const mapStateToProps = (state) => {
 export default compose(
   connect(mapStateToProps),
   firestoreConnect([
-    { collection: 'projects', orderBy: ['createdAt', 'desc']},
-    { collection: 'notifications', limit: 3, orderBy: ['time', 'desc']}
+    { collection: 'projects', orderBy: ['createdAt', 'desc'] },
+    { collection: 'notifications', limit: 3, orderBy: ['time', 'desc'] }
   ])
 )(Dashboard)
